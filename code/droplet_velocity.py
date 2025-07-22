e3b66a7aada24aef73763439d2250c451d686d4a
DejianQIU <d.qiu@theo.chemie.tu-darmstadt.de>
2025-07-22 16:46:03 +0200

    t range setting and plotting for bl_nb comparision

--- code/droplet_velocity.py
+++ code/droplet_velocity.py
@@ -85,7 +85,14 @@ def plot_velocity_comparison(data, comparison_type, fixed_params, varying_params
                 params['bl_nb'] = bl_nb
                 dataset = get_data_for_params(data, params)
                 if dataset is not None and vel_type in dataset:
-                    line, = plt.plot(dataset['Time'], dataset[vel_type], label=f'bl_nb={bl_nb}', linewidth=2)
+                    # Apply time range filter if specified
+                    t_min = 0
+                    t_max = max_time
+                    if time_ranges and bl_nb in time_ranges:
+                        t_min, t_max = time_ranges[bl_nb]
+                    mask = (dataset['Time'] >= t_min) & (dataset['Time'] <= t_max)
+                    line, = plt.plot(dataset['Time'][mask], dataset[vel_type][mask], label=f'bl_nb={bl_nb}',
+                                     linewidth=2)
                     data_lines.append(line)
             title = f"xp={fixed_params['xp']}, Np={fixed_params['Np']}, mcl={fixed_params['mcl']}, v={fixed_params['v']}, wgh={fixed_params['wgh']}, pw={fixed_params['pw']}"
 
