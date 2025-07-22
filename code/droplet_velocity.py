e3799861d88cd44d2ab45070a90e65615f15d88c
DejianQIU <d.qiu@theo.chemie.tu-darmstadt.de>
2025-07-22 16:46:26 +0200

    deleting the old plot code for bl_nb comparison

--- code/droplet_velocity.py
+++ code/droplet_velocity.py
@@ -94,7 +94,7 @@ def plot_velocity_comparison(data, comparison_type, fixed_params, varying_params
                     line, = plt.plot(dataset['Time'][mask], dataset[vel_type][mask], label=f'bl_nb={bl_nb}',
                                      linewidth=2)
                     data_lines.append(line)
-            title = f"xp={fixed_params['xp']}, Np={fixed_params['Np']}, mcl={fixed_params['mcl']}, v={fixed_params['v']}, wgh={fixed_params['wgh']}, pw={fixed_params['pw']}"
+                title = f"xp={fixed_params['xp']}, Np={fixed_params['Np']}, mcl={fixed_params['mcl']}, v={fixed_params['v']}, wgh={fixed_params['wgh']}, pw={fixed_params['pw']}"
 
         elif comparison_type == 'pw':
             for pw in varying_params:
